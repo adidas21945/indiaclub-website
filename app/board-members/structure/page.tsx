2d6a7e0980d4db5dbff0b1b342397fc17ea0f954
Aadi Dash <aadidash@MacBook-Pro-8.local>
2026-02-19 14:18:02 -0500

    Changed footer border

--- app/board-members/structure/page.tsx
+++ app/board-members/structure/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Structure() {
   return (
-    <section className="w-full">
+    <section className="w-full bg-white -mb-12 pb-12">
       <Image
         src="/images/BoardStructure.png"
         alt="India Club board structure"
         width={2000}
         height={1200}
         priority
-        className="w-full h-auto"
+        className="block w-full h-auto"
       />
     </section>
   );
